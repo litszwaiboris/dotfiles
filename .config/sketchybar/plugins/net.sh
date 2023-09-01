b010e98f49411d611bd41ca5f3767c1c758c071b
litszwaiboris <litszwaiboris@yahoo.com>
2023-09-01 15:47:27 -0400

    Updated .gitignore and script

--- .config/sketchybar/plugins/net.sh
+++ .config/sketchybar/plugins/net.sh
@@ -1,10 +1,15 @@
-LABEL=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk 'NR==13 {print $2}')
+LABEL=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk 'NR==13 {$1 = ""; print}' | sed 's/^ //g')
 
-get=$(networksetup -getairportpower en0)
+get=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '{print $2}')
 
-if [[ $get == *"On"* ]]; then
+connection=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk 'NR==5 {print $2}')
+
+if [[ $connection == "running" ]]; then
 	sketchybar -m --set $NAME icon="󰖩 " \
 	                          label.drawing=on
+elif [ $connection == "init" ] || [ $connection == "scanning" ]; then
+    sketchybar -m --set $NAME icon="󱚵 " \
+	                          label.drawing=off
 fi
 
 if [[ $get == *"Off"* ]]; then
@@ -12,4 +17,4 @@ if [[ $get == *"Off"* ]]; then
 	                          label.drawing=off
 fi
 
-sketchybar -m --set $NAME label=$LABEL
+sketchybar -m --set $NAME label="$LABEL"
