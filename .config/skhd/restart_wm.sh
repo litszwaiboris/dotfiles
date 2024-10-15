4e5fc8f0de28fe689e634f96cb45ed8042720c82
Boris Li <72115911+litszwaiboris@users.noreply.github.com>
2024-10-15 02:32:23 -0400

    Delete .config/skhd directory

--- .config/skhd/restart_wm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-osascript -e 'display notification "Restarting Environment" with title "Windows Manager"'
-killall Dock
-killall Finder
-yabai --restart-service
-skhd --restart-service
-brew services restart sketchybar
-brew services restart borders
