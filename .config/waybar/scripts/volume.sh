e339a8a06f750b4a786d0238cdf2afa34877ebe4
Boris Li <72115911+litszwaiboris@users.noreply.github.com>
2024-10-17 19:52:06 -0400

    Update volume.sh

--- .config/waybar/scripts/volume.sh
+++ .config/waybar/scripts/volume.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}' | sed 's/0.0//' | sed 's/0.//')
 mode=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $3}')
 
 if [[ ! -z  $mode ]]; then
     echo "  Muted"
 else
-    echo "  $volume"
+    echo "  $volume%"
 fi
